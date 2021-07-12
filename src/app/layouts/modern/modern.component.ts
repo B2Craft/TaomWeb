a0064ce9af388bc7420af987b4f1702c5b5b8fe9
Buğra Güney <bugraguney@msn.com>
2021-07-13 00:02:51 +0300

    taom conversion

--- src/app/layouts/modern/modern.component.ts
+++ src/app/layouts/modern/modern.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router';
-
+import { DeviceDetectorService } from 'ngx-device-detector';
 
 @Component({
   selector: 'app-modern',
@@ -11,11 +11,27 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ModernComponent implements OnInit {
 
+  appStoreUrl = "https://apps.apple.com/tr/app/a%C5%9Fk%C4%B1m/id1533857070";
+  playStoreUrl = "https://play.google.com/store/apps/details?id=com.askimapp.android";
   constructor(private route: ActivatedRoute,
-    private title: Title) { }
+    private title: Title, 
+    private deviceService: DeviceDetectorService) { }
 
   ngOnInit() {
     this.title.setTitle(this.route.snapshot.data['title']);
+
+    // this.deviceService.os
+    this.route.queryParams.subscribe(params => {
+      let abc = params["swipe"];
+      // console.log(abc);
+      if (abc) {
+        if (this.deviceService.os === "Android") {
+          window.location.href = this.playStoreUrl;
+        } else if (this.deviceService.os === "iOS") {
+          window.location.href = this.appStoreUrl;
+        } 
+      }
+    });
   }
 
 }
